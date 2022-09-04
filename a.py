22d809ccd2d002d7d0f46b0ace81e0fbea3bcb89
wxf <1774694862@qq.com>
2022-09-04 19:22:26 +0800

    22

---
+++ a.py
@@ -0,0 +1,5 @@
+def test(a: int, b: int, c: int, d: int, e: int, f: int, g: int, h: int) -> int:
+    if a == 1:
+        return a + b + c + d + e + f + g + h
+    else:
+        return a + b + c + d + e + f + g + h + 1
